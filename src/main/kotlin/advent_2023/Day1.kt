3e29bd1fcd35424ba5ffba3017aebdaa7d3fa326
cloroff <christian.loroff@capgemini.com>
2023-12-01 10:30:58 +0100

    correct implementation for day 1

--- src/main/kotlin/advent_2023/Day1.kt
+++ src/main/kotlin/advent_2023/Day1.kt
@@ -1,7 +1,28 @@
 package advent_2023
 
-class Day1 {
-    fun process(args: Array<String>) {
+import java.io.FileReader
 
+class Day1 {
+    private val spelledDigits = listOf("one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
+
+    fun process(args: Array<String>) {
+        val calibrationDocument = FileReader(args[0]).readLines()
+        println("Input: $calibrationDocument")
+
+        println("Part1: " + calibrationDocument.map { character -> character.filter { it.isDigit() } }
+            .sumOf { ("" + it.first() + it.last()).toInt() })
+
+        println("Part2: " + calibrationDocument.map { replaceNumbers(it) }
+            .map { character -> character.filter { it.isDigit() } }
+            .sumOf { ("" + it.first() + it.last()).toInt() })
+    }
+
+    private fun replaceNumbers(input: String) : String {
+        var output = input
+        val firstFind = output.findAnyOf(spelledDigits)
+        firstFind?.let { output = output.replace(it.second, (spelledDigits.indexOf(it.second)+1).toString()) }
+        val lastFind = output.findLastAnyOf(spelledDigits)
+        lastFind?.let { output = output.replace(it.second, (spelledDigits.indexOf(it.second)+1).toString()) }
+        return output
     }
 }
