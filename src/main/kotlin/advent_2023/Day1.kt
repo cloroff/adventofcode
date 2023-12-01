97d70ff55baea18961f8509b1b61833208aa6f3a
cloroff <christian.loroff@capgemini.com>
2023-12-01 17:08:04 +0100

    improved readability

--- src/main/kotlin/advent_2023/Day1.kt
+++ src/main/kotlin/advent_2023/Day1.kt
@@ -9,20 +9,22 @@ class Day1 {
         val calibrationDocument = FileReader(args[0]).readLines()
         println("Input: $calibrationDocument")
 
-        println("Part1: " + calibrationDocument.map { character -> character.filter { it.isDigit() } }
+        println("Part1: " + calibrationDocument
+            .map { character -> character.filter { it.isDigit() } }
             .sumOf { ("" + it.first() + it.last()).toInt() })
 
-        println("Part2: " + calibrationDocument.map { replaceNumbers(it) }
+        println("Part2: " + calibrationDocument
+            .map { replaceNumbers(it) }
             .map { character -> character.filter { it.isDigit() } }
             .sumOf { ("" + it.first() + it.last()).toInt() })
     }
 
     private fun replaceNumbers(input: String) : String {
         var output = input
-        val firstFind = output.findAnyOf(spelledDigits)
-        firstFind?.let { output = output.replace(it.second, (spelledDigits.indexOf(it.second)+1).toString()) }
-        val lastFind = output.findLastAnyOf(spelledDigits)
-        lastFind?.let { output = output.replace(it.second, (spelledDigits.indexOf(it.second)+1).toString()) }
+        val firstFind = output.findAnyOf(spelledDigits)?.second
+        firstFind?.let { output = output.replace(it, (spelledDigits.indexOf(it)+1).toString()) }
+        val lastFind = output.findLastAnyOf(spelledDigits)?.second
+        lastFind?.let { output = output.replace(it, (spelledDigits.indexOf(it)+1).toString()) }
         return output
     }
 }
